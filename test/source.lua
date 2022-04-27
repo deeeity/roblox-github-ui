bf31dec37d12bc679ead26ce750f5966eec5f473
Deity <pavlov.dalibor@hotmail.com>
2022-04-27 19:21:11 +0200

    test directory

--- test/source.lua
+++ test/source.lua
@@ -1,4 +1,4 @@
-loadstring(game:HttpGet("https://raw.githubusercontent.com/deeeity/roblox-github-ui/master/init.lua", true))()
+loadstring(game:HttpGet("https://raw.githubusercontent.com/deeeity/roblox-github-ui/master/init.lua", true))("deeeity", "roblox-github-ui", "master")
 
 local main = GHUI.Components.Core("Main")
 print(main.name)
