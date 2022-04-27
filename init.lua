27307f0c339112e79ad58510117677f73ca8d06e
Deity <pavlov.dalibor@hotmail.com>
2022-04-27 19:10:35 +0200

    attempt 2

--- init.lua
+++ init.lua
@@ -1,28 +1,21 @@
 -- Ready the environment for the library
-local function init()
-    print("Test 1")
-    getgenv().GHUI = getgenv().GHUI or {}
-    if GHUI.Exists == nil then
-        print("Test 2")
-        GHUI.Exists = true
-        GHUI.Version = 1.0
-        GHUI.Debug = false
-        GHUI.Fork = "https://github.com/deeeity/roblox-github-ui/blob/master/"
-        GHUI.ObjectRegister = {}
-        GHUI.Include = function(link : string)
-            return loadstring(game:HttpGet(GHUI.Fork .. link:lower() .. ".lua", true))()
-        end
-
-        -- Load the components
-        GHUI.Components = {
-            "Base" = GHUI.Include("components/base"),
-            "Core" = GHUI.Include("components/core")
-        }
-        print("Test 3")
+print("Test 1")
+getgenv().GHUI = getgenv().GHUI or {}
+if GHUI.Exists == nil then
+    print("Test 2")
+    GHUI.Exists = true
+    GHUI.Version = 1.0
+    GHUI.Debug = false
+    GHUI.Fork = "https://github.com/deeeity/roblox-github-ui/blob/master/"
+    GHUI.ObjectRegister = {}
+    GHUI.Include = function(link : string)
+        return loadstring(game:HttpGet(GHUI.Fork .. link:lower() .. ".lua", true))()
     end
 
-    print("Test 4")
-    return GHUI
+    -- Load the components
+    GHUI.Components = {
+        "Base" = GHUI.Include("components/base"),
+        "Core" = GHUI.Include("components/core")
+    }
+    print("Test 3")
 end
-
-return init()
